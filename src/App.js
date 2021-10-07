c4eac8ab8d5896edd874222666d37578ee9ba7b2
TonySauceda <asauceda@boz.mx>
2021-10-07 13:11:21 -0500

    Agregar estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Formik, Form, Field } from 'formik'
 import { useState } from 'react'
 import './header.css'
+import './content.css'
+import './article.css'
 
 const App = () => {
 	const [photos, setPhotos] = useState([])
@@ -25,7 +27,6 @@ const App = () => {
 
 						const data = await response.json()
 						setPhotos(data.results)
-						console.log(photos)
 					}}
 				>
 					<Form>
@@ -38,7 +39,8 @@ const App = () => {
 					{photos.map((photo) => (
 						<article key={photo.id} onClick={() => open(photo.links.html)}>
 							<img src={photo.urls.regular} alt={photo.alt_description} />
-							<p>{[photo.description, photo.alt_description].join('-')}</p>
+							<p>{photo.description ? photo.description : photo.alt_description}</p>
+							<small>{photo.user.name}</small>
 						</article>
 					))}
 				</div>
