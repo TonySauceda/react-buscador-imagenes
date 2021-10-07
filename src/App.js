f02e60c55adae6c1b25e2a6a0b97e6289aed3de8
TonySauceda <asauceda@boz.mx>
2021-10-07 12:17:51 -0500

    Agregar header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import './header.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<header>
+				<Formik
+					initialValues={{ search: '' }}
+					onSubmit={async (values) => {
+						// Llamar a la api de unsplash
+						console.log(values)
+					}}
+				>
+					<Form>
+						<Field name="search" />
+					</Form>
+				</Formik>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
